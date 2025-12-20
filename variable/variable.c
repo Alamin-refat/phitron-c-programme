22bf8c3011c75dca23cad5a11cfde44a4412a832
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-20 12:06:10 +0600

    what is variable and how to use

---
+++ variable/variable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int price = 50 + 60;
+    int marks =90;
+
+    printf("%d\n",price);
+    printf("%d\n",marks);
+
+    marks = price - 10;
+
+    printf("%d\n",marks);
+
+
+
+    return 0;
+
+
+
+}
