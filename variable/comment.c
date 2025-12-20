5f435b3615f8925f091045549eb0c88f3535ed25
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-20 12:31:48 +0600

    comment type and use

---
+++ variable/comment.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int price = 50 + 60; //this is single line comment..........
+    int marks =90;
+
+    printf("%d\n",price);
+    printf("%d\n",marks);    /*this is
+                                multi line comment */
+
+    marks = price - 10;
+
+    printf("%d\n",marks);
+
+
+
+
+
+
+
+}
