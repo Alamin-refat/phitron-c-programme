62c1c3e2d4d4cc05d377dd564a5bbd8effbdb3b0
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-22 16:59:42 +0600

    input from users

---
+++ variable data type/input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+   int english_marks,math_marks;
+
+   printf("Enter in english and math marks: ");
+   scanf("%d %d",&english_marks, &math_marks);
+   int total_marks = english_marks+math_marks;
+   printf("Total marks = %d",total_marks);
+
+
+    return 0;
+
+
+
+
+}
