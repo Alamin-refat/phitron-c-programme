db027e968b98f32ddcf2910837901522b130df1d
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-22 16:19:43 +0600

    different type data type

---
+++ variable data type/data type.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int price =100000000;
+    printf("price = %d\n",price);
+
+    float temp = 32.5;
+    printf("temp = %f\n",temp);
+
+    double temp2 = 30.5;
+    printf("temp2 = %lf\n",temp2);
+
+    char cha='G';
+    printf("cha = %c\n",cha);
+
+}
