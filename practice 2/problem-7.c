88885ed527809d558fe4aeb91c371cf89e9004c9
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-29 17:15:27 +0600

    convert to farenhiet

---
+++ practice 2/problem-7.c
@@ -0,0 +1,25 @@
+/*Write a C program to input temperature in Centigrade and convert to Fahrenheit. Example
+Input
+Enter temperature in Celsius = 100
+Output
+Temperature in Fahrenheit = 212.0 F
+
+	Hint: Temperature conversion formula from degree Celsius to Fahrenheit is given by
+*/
+
+#include<stdio.h>
+
+int main(){
+
+    int c;
+    printf("Enter temperature in Celsius =  ");
+    scanf("%d",&c);
+    float F = (c*9/5)+32;
+    printf("Temperature in Fahrenheit = %.1f F\n",F);
+
+
+    return 0;
+
+
+
+}
