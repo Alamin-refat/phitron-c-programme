3e55b60d03581a99985b0e6eb0e679bf0ec24412
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-14 06:06:49 +0600

    if-else problem solve

---
+++ Week 2/control flow in c/if-else.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+int marks;
+scanf("%d",&marks);
+
+if(marks >= 40){
+    printf("you have passed\n");
+    printf("Congrats\n");
+}
+
+else {
+    printf("you have failed\n");
+}
+
+
+
+return 0;
+
+
+}
