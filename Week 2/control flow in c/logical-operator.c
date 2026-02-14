bb03c4b48ba2efdcb59147bc803443e9f05d03e2
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-14 17:38:01 +0600

    logical operator

---
+++ Week 2/control flow in c/logical-operator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int num;
+    scanf("%d", &num);
+
+
+    if (num >= 0 && num <= 10){
+        printf("okay\n");
+    }
+
+    else {
+        printf("not okay\n");
+    }
+
+
+
+    return 0;
+
+
+
+
+}
