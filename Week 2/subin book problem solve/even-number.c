a04348018e03ce207f29870b3a5859d779fef3a5
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-15 10:49:39 +0600

    odd even number find

---
+++ Week 2/subin book problem solve/even-number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int num = 10;
+
+    if(num%2==0){
+        printf("even number\n");
+    }
+    else{
+        printf("odd number\n");
+    }
+    return 0;
+
+
+
+}
