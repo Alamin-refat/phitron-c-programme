c17c3a81b5588cf1791320c59c1f3c8156fed6cc
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-17 05:06:50 +0600

    loop-if problem solve

---
+++ Week 2/loop/loop-if.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int n=10;
+
+    for(int i=0;i<n;i++){
+        if(i%2 ==0){
+            printf("%d is even\n",i);
+        }
+        else{
+            printf("%d is odd\n",i);
+        }
+    }
+
+
+
+
+
+
+}
