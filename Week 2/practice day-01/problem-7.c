69e58bef98df9bce5f3fd52e8c435c788180cdf9
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-18 20:56:00 +0600

    find large number problem solve

---
+++ Week 2/practice day-01/problem-7.c
@@ -0,0 +1,26 @@
+/*দুটি সংখ্যা এর মধ্যে বড়টি বের করার জন্য প্রোগ্রাম লিখ।*/
+
+
+
+#include<stdio.h>
+
+
+int main(){
+
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a>b){
+        printf("%d is greater than %d", a,b);
+    }
+    else{
+        printf("%d is greater than %d",b,a);
+    }
+
+
+    return 0;
+
+
+
+
+}
