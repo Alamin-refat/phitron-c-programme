cade402d9fb55e5e69a69c86e08c7d412dfa5275
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-19 15:20:18 +0600

    nested loop pattern type problem solve

---
+++ Week 2/practice day-01/problem-10a.c
@@ -0,0 +1,31 @@
+/* 1
+   22
+   333
+   4444
+*/
+
+#include<stdio.h>
+
+
+int main(){
+
+    int rows=4;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+
+
+    }
+    return 0;
+
+
+
+
+
+
+
+
+}
