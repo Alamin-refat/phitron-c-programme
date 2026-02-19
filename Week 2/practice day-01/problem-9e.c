bc77b0bd7519b4889f01f1b8cf72f3f72a9cb22e
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-19 15:06:57 +0600

    sum of number series

--- Week 2/practice day-01/problem-9e.c
+++ Week 2/practice day-01/problem-9e.c
@@ -1,1 +1,23 @@
+/*100 + 97 + 94 + 91 + ….  (0 এর চেয়ে বড় পর্যন্ত)   ধারা গুলোর যোগফল বের করার প্রোগ্রাম লিখ। */
 
+#include<stdio.h>
+
+
+int main(){
+
+    int sum=0;
+
+    for(int i=100;i>0;i=i-3){
+        sum= sum+i;
+
+    }
+    printf("total sum = %d",sum);
+
+    return 0;
+
+
+
+
+
+
+}
