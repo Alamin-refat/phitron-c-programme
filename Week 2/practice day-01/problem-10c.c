f094c95540dda33c5c5c831991348eabdad5c3f0
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-19 15:37:57 +0600

    Floyd's Triangle pattern problem solve

---
+++ Week 2/practice day-01/problem-10c.c
@@ -0,0 +1,31 @@
+/*
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+*/
+
+#include<stdio.h>
+
+
+int main(){
+
+    int rows=4;
+    int count=1;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",count);
+            count++;
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+
+
+
+}
