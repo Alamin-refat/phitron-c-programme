6f2a891588936046c25a5daca6e069b8b9c5fa11
Alamin Refat <alaminrefat2017@gmail.com>
2026-02-24 16:20:14 +0600

    find cube problem solve

---
+++ Week 2/practice day-02/problem-10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, n, cube;
+
+
+    printf("Input number of terms : ");
+    scanf("%d", &n);
+
+
+    for (i = 1; i <= n; i++) {
+        cube = i * i * i;
+        printf("Number is : %d and cube of the %d is :%d\n", i, i, cube);
+    }
+
+    return 0;
+}
